9bab85c7c4bd6d6c11e2d9d3e2afcf522fbdea89
alexm <alexmicle@gmail.com>
2017-02-28 20:33:12 +0200

    --lab4 changes

---
+++ src/Person.java
@@ -0,0 +1,35 @@
+import java.util.Date;
+
+/**
+ * Created by alexm on 2/28/2017.
+ */
+public class Person {
+
+    private String prenume;
+    private String nume;
+    private char gender;
+    private int ziNastere;
+
+    public String getPrenume(){
+        return prenume;
+    }
+
+    public void setPrenume(String prenume){
+
+        this.prenume = prenume;
+    }
+
+    public void setNume(String nume){
+        this.nume = nume;
+    }
+
+    public String getName(){
+        return nume;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
--- src/ReadName.java
+++ src/ReadName.java
@@ -9,15 +9,17 @@ public class ReadName {
 
         String[] name = new String[n];
         String[] surname = new String[n];
+        int[] ziuaDeNastere = new int[n];
 
         for(int i =0; i<n; i ++){
 
             int j= i+1;
             name[i] = SkeletonJava.readStringConsole("Name" + j + "?");
             surname[i] = SkeletonJava.readStringConsole("Surname" + j + "?");
+            ziuaDeNastere[i] = SkeletonJava.readIntConsole("Ziua de nasere?Luni=0");
 
             int len = surname[i].length();
-            System.out.println(surname[i] + "+" + name[i] + " " + len);
+            System.out.println(surname[i] + "+" + name[i] + " " + len + "nas" + ziuaDeNastere[i]);
 
         }
     }
